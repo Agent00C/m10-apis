af2baaddb49ec80bdbf62021c10c056bcfe0a6f0
Agent00C <j2stx@yahoo.com>
2016-10-25 14:51:31 -0700

    Completed Exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,18 +8,43 @@ setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
-
+TopTenTracks <- function(my_artist) {
+  base <- 'https://api.spotify.com/v1/'
+  
+  search <- paste0('search?q=', my_artist, '&type=artist')
+  
+  artist.url <- paste0(base,search)
+   
+  artist.data <- fromJSON(artist.url)
+  
+  artist.id <- artist.data$artists$items$id[1]
+  
+  album.query <- paste0(base, 'artists/', artist.id, '/top-tracks?country=US')
+  
+  top.tracks <- fromJSON(album.query)
+  
+  return(top.tracks$tracks)
+}
 
 
 # What are the top 10 tracks by Nelly?
-
+TopTenTracks('Nelly')
 
 
 
 ### Bonus ### 
 # Write a function that allows you to specify a search type (artist, album, etc.), and a string, 
 # that returns the album/artist/etc. page of interest
+SearchTypeFor <- function(type, string) {
 
+  base <- 'https://api.spotify.com/v1/'
+  
+  search <- paste0(base, 'search?', 'q=', string, '&type=', type)
+  
+  artist.info <- fromJSON(search)
+  
+  return(artist.info)
+}
 
 # Search albums with the word "Sermon"
-
+SearchTypeFor('album', 'Sermon')
